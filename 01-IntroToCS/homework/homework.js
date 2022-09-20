73d5c315b6286414f5e4421457ee54f9aaf15a27
fidel acevedo <acv2fidel@gmail.com>
2022-09-19 21:52:22 -0400

    homework-ok

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,21 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    sum += num[i] * Math.pow(2, num.length - i - 1);
+  }
+  return sum;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+let binario = '';
+  while (num > 0) {
+    binario = (num % 2) + binario;
+    num = Math.floor(num / 2);
+  }
+  return binario;
 }
 
 
