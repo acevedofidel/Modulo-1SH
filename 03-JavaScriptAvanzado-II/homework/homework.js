bc6dbb104fa62002e5e8835e87a6d4415148b0d7
acevedofidel <acv2fidel@gmail.com>
2022-09-29 16:33:59 -0300

    addChanges

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -3,7 +3,7 @@
 // Closures
 
 function counter() {
-  /*
+ /*
   Ejercicio 1
 
   La función counter debe retornar otra función. Esta función retornada debe actuar como un contador, retornando un valor numérico que empieza en 1 e incrementa con cada invocación.
@@ -19,6 +19,10 @@ function counter() {
   otroContador()      // 2
   otroContador()      // 3
    */
+  var cont = 1;
+  return function () {
+    return cont++;
+  }
 }
 
 function cacheFunction(cb) {
@@ -41,6 +45,15 @@ function cacheFunction(cb) {
   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
 
   */
+let cache = {};
+  return function (arg) {
+    if (cache.hasOwnProperty(arg)) {
+      return cache[arg];
+    } else {
+      cache[arg] = cb(arg);
+      return cache[arg];
+    }
+  }
 }
 
 // Bind
@@ -67,8 +80,8 @@ function getNombre() {
   Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-let getNombreInstructor;
-let getNombreAlumno;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 /*
   Ejercicio 4
@@ -80,9 +93,9 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
   return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-let textoAsteriscos;
-let textoGuiones;
-let textoUnderscore;
+let textoAsteriscos = crearCadena.bind(null, "*", "*");
+let textoGuiones = crearCadena.bind(null, "-", "-");
+let textoUnderscore = crearCadena.bind(null, "_", "_");
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
